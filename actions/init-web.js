1347dc13e98237bf053b21b1d01785da97a1e9e9
jimmybae <mywindbell@gmail.com>
2018-04-25 14:00:33 +0900

    Modify question check

--- actions/init-web.js
+++ actions/init-web.js
@@ -13,12 +13,13 @@ module.exports = (resolve = undefined, reject = undefined) => {
   let question = questions.replaceWebFolder;
   let deleteFlag = true;
   
-  /* index.js의 isCordova()에서 www folder가 존재하는지 검사하므로 아래 로직 불필요
+  /*
+    index.js의 isCordova()에서 www folder가 존재하는지 검사하므로 아래 로직 불필요
+  */
   if (!fs.existsSync(wwwDir)) {
     question = questions.createWebFolder;
     deleteFlag = false;
   }
-  */
   inquirer.prompt(question)
   .then(answers => {
     if(answers.answer) {
